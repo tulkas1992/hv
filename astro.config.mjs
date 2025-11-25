499d052007eec27273d239d96998d9aeb4eb7a54
VIctor RUiz <vr24355@gmail.com>
2025-11-25 09:12:38 -0500

    Minor fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://efeele.dev",
+  site: "victor-ruiz-reyes.info",
   integrations: [preact(), icon(), sitemap()],
 
   vite: {
